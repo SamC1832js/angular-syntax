3c8e30d34e0f8caf70ab250288cfe94e42837992
sam156123328 <sam18328578207@gmail.com>
2025-01-25 19:35:07 -0600

    refactor: 移除占位符并添加新功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-syntax';
+  onClickMethod(value: any) {
+    console.log(value);
+  }
+
+  username: string = 'aaa';
+  phone: string = '123456';
+  isActive: boolean = false;
+  toggleactive() {
+    if (this.isActive == false) {
+      this.isActive = true;
+      console.log('true');
+    } else {
+      this.isActive = false;
+      console.log('false');
+    }
+  }
 }
